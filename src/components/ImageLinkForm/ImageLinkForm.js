713ad4bdd115eafd061f77cb107f67ac5cdcbd54
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-08-01 17:40:01 +0200

    adjusted text on main page

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className="f3">
-                {'This Magic Brain will detect faces in you pictures. Give it a try.'}
+                {'Enter a picture\'s URL and the AI will mark the face!'}
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
